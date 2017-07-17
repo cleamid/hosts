949eac2cbfff982decac0ab6ab86ea03bb9509f5
Layne <cleamid@gmail.com>
2017-07-17 19:31:06 +0800

    linux一键脚本

--- reset_host.sh
+++ reset_host.sh
@@ -1,3 +1,8 @@
+cd ~/Download
+wget ftp://35.167.203.34/hosts -O ~/Downloads/hosts
+
 echo 127.0.0.1 liveupdate.mac.sandai.net >> ~/Downloads/hosts
+
 sudo cp ~/Downloads/hosts /etc/hosts
-killall -HUP mDNSResponder
+sudo killall -HUP mDNSResponder
+rm ~/Downloads/hosts
